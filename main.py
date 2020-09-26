447e96ed15da8d65c0e5f28c367dfd6d96635b56
ajitalhatimi <ajit@alhatimi.com>
2020-09-26 15:41:16 +0530

    send mail goolse function done

--- main.py
+++ main.py
@@ -7,22 +7,13 @@ import smtplib
 import boto3
 from botocore.exceptions import ClientError
 import pyjade
-# from email.mime.text import MIMEText
-# from email.mime.multipart import MIMEMultipart
 from jinja2 import Environment, FileSystemLoader
 from flask import render_template
 import os
 
-env = Environment(
-    loader=FileSystemLoader('%s/templates/' % os.path.dirname(__file__)))
+SENDER = "Source.SupplyVan.com (AL HATIMI Trading LLC)<source@supplyvan.com>"
 
-# Replace sender@example.com with your "From" address.
-# This address must be verified with Amazon SES.
-SENDER = "Sender Name <source@supplyvan.com>"
-
-# Replace recipient@example.com with a "To" address. If your account 
-# is still in the sandbox, this address must be verified.
-RECIPIENT = "ajit@alhatimi.com"
+# RECIPIENT = "ajit@alhatimi.com"
 
 # Specify a configuration set. If you do not want to use a configuration
 # set, comment the following variable, and the 
@@ -31,40 +22,19 @@ RECIPIENT = "ajit@alhatimi.com"
 
 # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
 AWS_REGION = "us-east-1"
-
-
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
-attributesList = {
-    "brands": ["brand1", "brand2", "brand3", "brand4", "brand5"],
-    "category": ["category1", "category2", "category3", "category4", "category5"]
-}
-URL = 'https://user-service-dot-turing-terminus-224612.el.r.appspot.com/app/signup'    
+URL = 'https://user-service-dot-turing-terminus-224612.el.r.appspot.com/api/find-user-by-brand-category    
+# URL = 'http://localhost:2000/api/find-user-by-brand-category'    
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 # The subject line for the email.
-SUBJECT = "Amazon SES Test (SDK for Python)"
+SUBJECT = "Receive RFX"
 
 # The email body for recipients with non-HTML email clients.
-BODY_TEXT = ("Amazon SES Test (Python)\r\n"
-             "This email was sent with Amazon SES using the "
-             "AWS SDK for Python (Boto)."
-            )
-            
-# The HTML body of the email.
-# BODY_HTML = """<html>
-# <head></head>
-# <body>
-#   <h1>Amazon SES Test (SDK for Python)</h1>
-#   <p>This email was sent with
-#     <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-#     <a href='https://aws.amazon.com/sdk-for-python/'>
-#       AWS SDK for Python (Boto)</a>.</p>
-# </body>
-# </html>
-#     """            
+BODY_TEXT = ("Receive RFX")
 
 # The character encoding for the email.
 CHARSET = "UTF-8"
@@ -72,18 +42,16 @@ CHARSET = "UTF-8"
 # Create a new SES resource and specify a region.
 client = boto3.client('ses',region_name=AWS_REGION)
 
-@pyjade.register_filter('capitalize')
-def capitalize(text,ast):
-  return text.capitalize()
+# @pyjade.register_filter('capitalize')
+# def capitalize(text,ast):
+#   return text.capitalize()
   
-def sendMail(BODY_HTML):
+def sendMail(BODY_HTML, recipient):
     try:
         #Provide the contents of the email.
         response = client.send_email(
             Destination={
-                'ToAddresses': [
-                    RECIPIENT,
-                ],
+                'ToAddresses': recipient,
             },
             Message={
                 'Body': {
@@ -115,45 +83,42 @@ def sendMail(BODY_HTML):
 
 @app.route('/sendMail', methods=['POST'])
 def prepareForSendMail():
-    request_json = request.json
-    print('request_json = ', request_json)
-    # f = open("reciveRFX.html", "r")
-    # BODY_HTML = f.read()
-    # print('file content  = ',f.read())
-    user_name = "Ajit Shinde"
-    rfx_name = "sending the mail for RFX send"
-    totalLineItems = "100"
-    titalQuantity = "100"
+    request_json = request.json 
+    # print('request_json = ', request_json)
+    # print('request_json.total_lineItems = ', request_json["total_lineItems"])
+    user_name = ""
+    rfx_name = request_json["rfx_name"]
+    totalLineItems = request_json["total_lineItems"]
+    titalQuantity = request_json["total_quantity"]
     BODY_HTML = render_template('reciveRFX.html', user_name=user_name, rfx_name=rfx_name, totalLineItems=totalLineItems, titalQuantity=titalQuantity)
-    print('==============================')
-    print(render_template('reciveRFX.html', user_name=user_name, rfx_name=rfx_name, totalLineItems=totalLineItems, titalQuantity=titalQuantity) )
-    print('==============================')
     
-    # print('converting the file = ',capitalize(f.read(), "ast") )
-    # try:
-    #     # getting mailids from related users having same brand and category
-    #     response = requests.post(URL, data = json.dumps(attributesList), headers = headers)
+    try:
+        # getting mailids from related users having same brand and category
+        response = requests.post(URL, data = json.dumps(request_json), headers = headers)
 
-    #     //email should send to the this data
-    #     response.raise_for_status()
-    # except requests.exceptions.HTTPError as errh:
-    #     print ("Http Error:",errh)
-    # except requests.exceptions.ConnectionError as errc:
-    #     print ("Error Connecting:",errc)
-    # except requests.exceptions.Timeout as errt:
-    #     print ("Timeout Error:",errt)
-    # except requests.exceptions.RequestException as err:
-    #     print ("OOps: Something Else",err) 
+    # email should send to the this data
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as errh:
+        print ("Http Error:",errh)
+    except requests.exceptions.ConnectionError as errc:
+        print ("Error Connecting:",errc)
+    except requests.exceptions.Timeout as errt:
+        print ("Timeout Error:",errt)
+    except requests.exceptions.RequestException as err:
+        print ("OOps: Something Else",err) 
 
     # print("api Responce response.status_code : ", response.status_code)
-    # print("api Responce response.status_code : ", response.text)
-    # print("api Responce : ", response.json())
+    json_res = json.loads( response.text )
+    # print("api Responce response.response.text: ", json.loads( response.text ))
     # Try to send the email.
-    sendMail(BODY_HTML)
+    recipient = []
+    for x in json_res:
+        # print(' =========single obj mail = ',x["email"])  
+        recipient.append(x["email"])  
+    # print("=========recipient = ",recipient)    
+    sendMail(BODY_HTML, recipient)
     
-    
-    return json.dumps(request_json)
-
+    return "emails sent successfully to all related vendors!!"
 
 app.run()
 
